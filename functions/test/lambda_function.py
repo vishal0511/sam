adb48ccc8acfd7e6b4ef2a371bac86ffa7e26570
vishal0511 <vishal.ladkani@gmail.com>
2019-02-21 16:09:02 +0530

    update

--- functions/test/lambda_function.py
+++ functions/test/lambda_function.py
@@ -4,13 +4,6 @@ import boto3
 def lambda_handler(event, context):
     #try:
     #    rds_client = boto3.client('rds')
-    #    user = "dbiamuser"
-    #    endpoint = "dev-aurora-api-cluster.cluster-chqgfrcmk2pe.us-east-1.rds.amazonaws.com"
-    ##    db_name = "infra_aws_api"
-     #   region= "us-east-1"
-     #   token = rds_client.generate_db_auth_token(endpoint,3306,user,region)
-     #   conn = pymysql.connect(host=endpoint, port=3306, user=user, password=token, db=db_name, auth_plugin_map={'mysql_cleartext_password':None}, cursorclass=pymysql.cursors.DictCursor)
-     #   print ("3")
     #except:
      #   print ("vikas inside exception block")
       #  e = sys.exc_info()
